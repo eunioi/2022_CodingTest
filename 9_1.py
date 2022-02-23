8651d2e8663908c3870de2afafeffc665e384470
eunioi <74131701+eunioi@users.noreply.github.com>
2022-02-23 21:07:33 +0900

    Add files via upload

---
+++ 9_1.py
@@ -0,0 +1,69 @@
+'''
+가장 간단한 알고리즘
+단계마다 '방문하지 않은 노드 중에서 최단 거리가 가장 짧은 노드를 선택'하기 위해 매 단계마다 1차원 리스트의 모든 원소를 확인(순차 탐색)한다.
+
+간단한 다익스트라 알고리즘 소스코드
+'''
+
+import sys
+input = sys.stdin.readline
+INF = int(1e9) ## 이건 무한을 의미하는 값으로 10억을 설정한 것
+
+print("노드의 개수, 간선의 개수를 입력하라")
+n, m = map(int, input().split())
+print("시작 노드 번호를 입력하라")
+start = int(input())
+
+## 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들어라
+graph = [[] for i in range(n+1)]
+## 방문한 적이 있는지 체크하는 목적의 리스트를 만들어라
+visited = [False] * (n+1)
+
+## 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n+1)
+
+print("모든 간선 정보를 입력받기")
+for _ in range(m):
+    print("a 노드에서 b 번 노드로 가는 비용이 c라는 의미로 3개 입력 띄어쓰기로 구분")
+    a, b, c = map(int, input().split())
+    graph[a].append((b,c))
+
+## 방문하지 않은 노드 중에서, 최단 거리가 짧은 노드의 번호를 반환
+def get_smallest_node():
+    min_value = INF
+    index = 0 ## 가잦ㅇ 최단 거리가 짧은 노드(인덱스)
+    for i in range(1, n+1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    ## 시작 노드에 대해서 초기화
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    ## 시작 노드를 제외한 전체 n-1 개의 노드에 대해 반복
+    for i in range(n-1):
+        ## 현재 최단 거리가 가장 짤븡ㄴ 꺼내서, 방문 처리
+        now = get_smallest_node()
+        visited[now] = True
+        ## 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now] :
+            cost = distance[now] + j[1]
+            ## 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧을 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+## 다익스트라 알고리즘 수행
+dijkstra(start)
+
+## 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n+1):
+    ## 도달 할 수 없는 경우, 무한(INFINITY) 이라고 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    ## 도달 할 수 있는 경우 거리를 출력
+    else:
+        print(f"도달 할 수 있는 거리 = {distance[i]}")
